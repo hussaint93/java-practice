da9562c460e6b8dff36d5d67f40a9c0bf87e4058
hussaint93 <tcssadriwalla@gmail.com>
2021-08-10 09:06:19 +0000

    more files added

--- src/com/tcs/entity/StaticDemo.java
+++ src/com/tcs/entity/StaticDemo.java
@@ -5,14 +5,18 @@ public class StaticDemo {
 		Bank bank = new Bank();
 		System.out.println(Bank.createNewAccountNumber());
 		System.out.println(Bank.createNewAccountNumber());
+		System.out.println(bank.instanceVarable);
 		Bank bank1 = new Bank();
 		System.out.println(bank1.createNewAccountNumber());
 		System.out.println(bank1.createNewAccountNumber());
+		System.out.println(bank1.instanceVarable);
 	}
 }
 class Bank{
 	static int counter;
+	int instanceVarable=10;
 	public static int createNewAccountNumber() {
 		return ++counter;
+		
 	}
 }
