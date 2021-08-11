5a33f120058f1af586f30c8f29e1e47e20683fc3
hussaint93 <tcssadriwalla@gmail.com>
2021-08-11 06:40:25 +0000

    static import part

---
+++ src/com/tcs/practice/FileReader.java
@@ -0,0 +1,14 @@
+package com.tcs.practice;
+//import static java.util.Arrays.*; // static import
+
+
+import java.util.Arrays;//normal import
+
+public class FileReader {
+private void maiin() {
+	// TODO Auto-generated method stub
+	int[] nums= {4,5,6,7,8};
+	//sort(nums); // works with static import 
+	Arrays.sort(nums);//works without static import.
+}
+}
