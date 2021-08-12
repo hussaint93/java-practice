984c9166bb506d439b4eb116cd698721a88cbcd1
hussaint93 <tcssadriwalla@gmail.com>
2021-08-12 06:14:12 +0000

    adding filtering to list using funtional programming

--- src/com/tcs/listdemo/ArraylistDemo.java
+++ src/com/tcs/listdemo/ArraylistDemo.java
@@ -4,10 +4,37 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class ArraylistDemo {
 	public static void main(String[] args) {
 		List<Integer> numbers = createList();
+		// sortList(numbers);
+		filterList(numbers);
+	}
+
+	/**
+	 * Method to filter the elements
+	 * 
+	 * @param numbers
+	 */
+	private static void filterList(List<Integer> numbers) {
+		numbers.forEach((number) -> {
+			System.out.println(number % 2 == 0);
+		});
+		List<Integer>filtered=numbers.stream()
+				.filter((number) -> number % 2 == 0)
+				.collect(Collectors.toList());
+		System.out.println(filtered);
+	}
+
+	/**
+	 * Method to sort the elements
+	 * 
+	 * @param numbers
+	 */
+	private static void sortList(List<Integer> numbers) {
 		System.out.println(numbers);
 		numbers.sort(new Comparator<Integer>() {
 			@Override
@@ -15,14 +42,14 @@ public class ArraylistDemo {
 				return o1 - o2;
 			}
 		});
-		System.out.println("after sorting "+numbers);
+		System.out.println("after sorting " + numbers);
 		numbers.sort(new Comparator<Integer>() {
 			@Override
 			public int compare(Integer o1, Integer o2) {
 				return o2 - o1;
 			}
 		});
-		System.out.println("after sorting in reverse order "+numbers);
+		System.out.println("after sorting in reverse order " + numbers);
 	}
 
 	private static List<Integer> createList() {
